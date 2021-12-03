8528ee4d925ee957576bf336f89878a064e83c82
kanji95 <kanishk5991@gmail.com>
2021-12-03 19:37:33 +0530

    calculate metrics

--- evaluate.py
+++ evaluate.py
@@ -1,5 +1,9 @@
 import torch
 import torch.nn as nn
+from torchvision.utils import save_image
+
+from statistics import mean
+from metrics import psnr, ssim
 
 @torch.no_grad()
 def validate(network, loader, epoch, log_interval=50):
@@ -8,7 +12,9 @@ def validate(network, loader, epoch, log_interval=50):
     
     criterion = nn.MSELoss()
     
-    total_loss = 0
+    total_loss = []
+    total_ssim = []
+    total_psnr = []
     
     for step, batch in enumerate(loader):
         x = batch.cuda()
@@ -17,8 +23,17 @@ def validate(network, loader, epoch, log_interval=50):
         
         loss = criterion(x_recon, x)
     
-        total_loss += loss.item()
+        total_loss += [loss.item()]
+        
+        ssim_ = ssim(x_recon, x)
+        psnr_ = psnr(x_recon, x)
+        
+        total_ssim += [ssim_]
+        total_psnr += [psnr_]
         
         if step % log_interval == 0:
-            print(f'Epoch: {epoch}, validation_loss: {total_loss/(step+1)}')
-    
+            print(f'Epoch: {epoch}, validation_loss: {mean(total_loss)}')
+            if epoch % 10 == 0:
+                save_image(x.detach().cpu(), f"orig_epoch_{epoch}_images.png")
+                save_image(x_recon.detach().cpu(), f"recon_epoch_{epoch}_images.png")
+    print(f'Validation Epoch: {epoch}, PSNR: {mean(total_psnr)}, SSIM: {mean(total_ssim)}')
